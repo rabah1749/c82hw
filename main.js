0b357a7e8712b2c1fcddac4d9d4e53f76b1564b8
rabah1749 <77684146+rabah1749@users.noreply.github.com>
2021-06-08 19:46:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+mouseEvent="";
+var last_x,last_y;
+  
+
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+color="black";
+
+canvas.addEventListener("mousedown",my_mousedown);
+function my_mousedown(e){
+    mouseEvent="mouseDown";
+}
+
+canvas.addEventListener("mouseleave",my_mouseleave);
+function my_mouseleave(e){
+    mouseEvent="mouseleave";
+}
+
+canvas.addEventListener("mouseup",my_mouseup);
+function my_mouseup(e){
+    mouseEvent="mouseup";
+}
+
+canvas.addEventListener("mousemove",my_mousemove);
+function my_mousemove(e){
+currentX=e.clientX-canvas.offsetLeft;
+currentY=e.clientY-canvas.offsetTop;
+if(mouseEvent=="mouseDown"){
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(currentX,currentY,20,0,2*Math.PI);
+ctx.stroke();
+}
+last_x=currentX;
+last_y=currentY;
+}
